c6cf620ee5f681cfcdecea1303c48cfee49c653e
unknown <knoflik21@seznam.cz>
2025-02-06 09:37:18 +0100

    Update pages

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,6 +1,3 @@
-import { getUsers } from "@/api/users"
-import Link from "next/link"
-
 export default function PostsPage() {
     return <h1>Posts page</h1>
   }
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,6 +1,3 @@
-import { getUsers } from "@/api/users"
-import Link from "next/link"
-
 export default function UsersPage() {
     return <h1>Users page</h1>
   }
