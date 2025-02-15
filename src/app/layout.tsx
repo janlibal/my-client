618ff4057cc1248a8561d77461836e9099ab0d7e
Jan Libal <libalj@seznam.cz>
2025-02-15 09:52:36 +0100

    Init Bands page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
           <div className="nav-text-large">smartBUY</div>
           <ul className="nav-list">
             <li>
+              <Link href="/bands">Bands</Link>
+            </li>
+            <li>
               <Link href="/products">Products</Link>
             </li>
             <li>
